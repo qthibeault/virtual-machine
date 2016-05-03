1392b3fc28350b97562f8bd3809dcf5655cb17de
Quinn Thibeault <quinn.thibeault96@gmail.com>
2016-05-02 22:42:25 -0700

    Fixed indentation

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 typedef enum {
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  PSH,
-  POP,
-  HLT
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    PSH,
+    POP,
+    HLT
 } InstructionSet;
 
+/* TODO I want to implement registers. Then I can push things into registers instead of the stack*/
+
 const int program[] = {
-  PSH, 10,
-  PSH, 4,
-  ADD,
-  POP,
-  HLT
+    PSH, 10,
+    PSH, 4,
+    ADD,
+    POP,
+    HLT
 };
 
 int stack[256];
@@ -21,32 +23,39 @@ int stack[256];
 int instruction_pointer = 0;
 int stack_pointer = 0;
 
+int fetch()
+{
+    return program[instruction_pointer++];
+}
+
 void eval(int instruction)
 {
-  switch(instruction)
-  {
-    case PSH: {
-      stack[stack_pointer++] = program[++instruction_pointer];
-    }
+    switch(instruction)
+    {
+        case PSH: {
+            stack[stack_pointer++] = program[++instruction_pointer];
+        }
 
-    case ADD: {
-      int a = stack[stack_pointer--];
-      int b = stack[stack_pointer--];
+        case ADD: {
+            int a = stack[stack_pointer--];
+            int b = stack[stack_pointer--];
 
-      int result = a + b;
-      stack[++stack_pointer] = result;
+            int result = a + b;
+            stack[++stack_pointer] = result;
+        }
+
+        /* TODO Other instructions here */
     }
-  }
 }
 
 int main()
 {
-  int instruction = 0;
-  while(instruction != HLT)
-  {
-    instruction = program[instruction_pointer++];
-    eval(instruction);
-  }
+    int instruction = 0;
+    while(instruction != HLT)
+    {
+        instruction = fetch();
+        eval(instruction);
+    }
 
-  return 0;
+    return 0;
 }
