78b5f50b9406d95c50e4cf2e3d520ffabd9fa8e9
Quinn Thibeault <quinn.thibeault96@gmail.com>
2016-05-02 22:31:44 -0700

    Implemented PSH instruction and built an evaluation loop

--- src/main.c
+++ src/main.c
@@ -16,7 +16,29 @@ const int program[] = {
   HLT
 };
 
+int stack[256];
+
+int instruction_pointer = 0;
+int stack_pointer = 0;
+
+void eval(int instruction)
+{
+  switch(instruction)
+  {
+    case PSH: {
+      stack[stack_pointer++] = program[++instruction_pointer];
+    }
+  }
+}
+
 int main()
 {
+  int instruction = 0;
+  while(instruction != HLT)
+  {
+    instruction = program[instruction_pointer++];
+    eval(instruction);
+  }
+
   return 0;
 }
