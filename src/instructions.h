2cf1353630e05f0b3a191e98608912c64357da5a
Quinn Thibeault <quinn.thibeault96@gmail.com>
2016-05-03 14:26:06 -0700

    Broke out instructions into header file

---
+++ src/instructions.h
@@ -0,0 +1,14 @@
+#ifndef __INSTR__
+#define __INSTR__
+
+typedef enum {
+    ADD,
+    SUB,
+    PRINT,
+    HLT,
+    NUM_OF_INSTRUCTIONS
+} InstructionSet;
+
+/* TODO move fetch and evaluate functions to this header file */
+
+#endif
